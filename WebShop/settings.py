190ec54484b73022798e53194ad27f83d454c248
wangk <405475123@qq.com>
2018-12-20 22:02:38 +0800

    add xadmin

--- WebShop/settings.py
+++ WebShop/settings.py
@@ -46,7 +46,9 @@ INSTALLED_APPS = [
     'DjangoUeditor',
     'goods.apps.GoodsConfig',
     'trade.apps.TradeConfig',
-    'user_operation.apps.UserOperationConfig'
+    'user_operation.apps.UserOperationConfig',
+    'crispy_forms',
+    'xadmin'
 ]
 
 MIDDLEWARE = [
@@ -118,7 +120,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
+
+TIME_ZONE = 'Asia/Shanghai'
 
 TIME_ZONE = 'UTC'
 
@@ -126,7 +130,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
